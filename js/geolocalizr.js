eca174dd590306a1d7f23790f805462f4fdd312b
Lucas Paiva <contacto.lucas.paiva@gmail.com>
2015-09-09 07:57:37 -0300

    Agrego funcionalidades2 de geolocalizacion html5

--- js/geolocalizr.js
+++ js/geolocalizr.js
@@ -5,48 +5,132 @@
  *
  */	
 
+//TODO: lo primero que debe evaluarse es la posicion geo , dependiendo de esto se agrega un elemento
+// al inicio del json , o no. De acuerdo a si esta disponible o no la geolocalizacion deberia llamar al 
+// metodo initializeNewMap
+
+var latInit, lngInit;
+var geoFlag = 0; 
+
+//Metodo que inicializa el mapa 
 function initializeNewMap(locations,zoom){
 
-	var map = new google.maps.Map(document.getElementById('googleMap'), {
-      zoom: zoom,
-      center: new google.maps.LatLng(locations[0]["lat"], locations[0]["lng"]),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+	console.log("inicializo mapa")
 
-    var infowindow = new google.maps.InfoWindow();
+	//aca pongo el codigo de geo html5 ?
+	if(navigator.geolocation){
+		//intentamos obtener las coordenadas del usuario
+		navigator.geolocation.getCurrentPosition(function(objPosicion){
+			//almacenamos en variables la longitud y latitud
+			geoFlag = 1;
+			var latAuto = objPosicion.coords.latitude;
+			var lngAuto = objPosicion.coords.longitude;
+			
+			console.log("Obtengo coordenadas");
+			console.log("LNG: " + lngAuto);
+			console.log("LAT: " + latAuto);
 
-    var marker, i;
+			console.log("antes de evaluar flag");
+			if(geoFlag == 1){
+				console.log("flag esta prenido");
+				latInit = latAuto;
+				lngInit = lngAuto;
+				console.log("Agrego el elemento");
+				locations.splice(0, 0,
+				    {
+						"lat" 			: latInit,
+						"lng"			: lngInit,
+						"title"			: "Es mi punto",
+						"description"	: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ",
+						"email"			: "",
+						"phone"			: "",
+						"img"			: "",
+						"link"			: "",
+						"bg-color"		: "cornflowerblue" 
+					}
+				);
+			}
+			console.log("despues de evaluar flag");
 
-    for (i = 0; i < locations.length; i++) {  
+			var map = new google.maps.Map(document.getElementById('googleMap'), {
+		      zoom: zoom,
+		      center: new google.maps.LatLng(locations[0]["lat"], locations[0]["lng"]),
+		      mapTypeId: google.maps.MapTypeId.ROADMAP
+		    });
 
-		marker = new google.maps.Marker({
-		position: new google.maps.LatLng(locations[i]["lat"], locations[i]["lng"]),
-		map: map,
-		icon: 'http://ktct.ca/images/gis/map_icon_poi.png'
+		    var infowindow = new google.maps.InfoWindow();
+
+		    var marker, i;
+
+		    console.log("recorro locations")
+		    for (i = 0; i < locations.length; i++) {  
+		    	console.log(i)
+				marker = new google.maps.Marker({
+				position: new google.maps.LatLng(locations[i]["lat"], locations[i]["lng"]),
+				map: map,
+				icon: 'http://ktct.ca/images/gis/map_icon_poi.png'
+				});
+
+				google.maps.event.addListener(marker, 'click', (function(marker, i) {
+				return function() {
+
+					var contentString = '<div id="containerInfoMap" style="width: 300px; height: 200px;">' +
+						'<div class="img" style="text-align: center; background-color: ' + locations[i]["bg-color"] +';"><img src="' + locations[i]["img"]  + '"></img></div>' +
+						'<div class="title" style="text-align: center;"><h3>' + locations[i]["title"] + '</h3></div>' +
+						'<div class="description" style="text-align: center;">' +
+						'<p>' + locations[i]["description"] + '</p>' +
+						'</div>' +
+						'<div class="phone" style="text-align: center;"><strong>Tel: </strong>' + locations[i]["phone"] + '</div>' +
+						'<div class="email" style="text-align: center;"><strong>Email: </strong>' + locations[i]["email"] + '</div>' +
+						'</div>';			            	
+
+					infowindow.setContent(contentString);
+					infowindow.open(map, marker);
+				}
+				})(marker, i));
+
+		    }
+
+		},function(objError){
+			//manejamos los errores devueltos por Geolocation API
+			switch(objError.code){
+				//no se pudo obtener la informacion de la ubicacion
+				case objError.POSITION_UNAVAILABLE:
+					geoFlag = 0;
+					console.log("La información de su posición no está disponible.");
+				break;
+				//timeout al intentar obtener las coordenadas
+				case objError.TIMEOUT:
+					geoFlag = 0;
+					console.log("Tiempo de espera agotado.");
+				break;
+				//el usuario no desea mostrar la ubicacion
+				case objError.PERMISSION_DENIED:
+					geoFlag = 0;
+				    console.log("Acceso denegado.")
+				break;
+				//errores desconocidos
+				case objError.UNKNOWN_ERROR:
+					geoFlag = 0;
+					console.log("Error desconocido.");
+				break;
+			}
 		});
+	}else{
+		//el navegador del usuario no soporta el API de Geolocalizacion de HTML5
+		geoFlag = 0;
+		console.log("Su navegador no soporta Geolocalizacion");
+	}
 
-		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-		return function() {
+	
+	
 
-			var contentString = '<div id="containerInfoMap" style="width: 300px; height: 200px;">' +
-				'<div class="img" style="text-align: center; background-color: ' + locations[i]["bg-color"] +';"><img src="' + locations[i]["img"]  + '"></img></div>' +
-				'<div class="title" style="text-align: center;"><h3>' + locations[i]["title"] + '</h3></div>' +
-				'<div class="description" style="text-align: center;">' +
-				'<p>' + locations[i]["description"] + '</p>' +
-				'</div>' +
-				'<div class="phone" style="text-align: center;"><strong>Tel: </strong>' + locations[i]["phone"] + '</div>' +
-				'<div class="email" style="text-align: center;"><strong>Email: </strong>' + locations[i]["email"] + '</div>' +
-				'</div>';			            	
 
-			infowindow.setContent(contentString);
-			infowindow.open(map, marker);
-		}
-		})(marker, i));
 
-    }
     
 }
 
+//Metodo que bindea el mapa
 function bindGoogleMaps(points,zoom){
 	google.maps.event.addDomListener(window, 'load', initializeNewMap(points,zoom));
 }
