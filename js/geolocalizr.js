56232960fe300cb1857ca2670d4a4c1a3e3ae2e5
Lucas Paiva <contacto.lucas.paiva@gmail.com>
2015-09-09 23:14:06 -0300

    - Integracion de geolocalizacion con bindeo de mapa. - mejoras de callback - ubicacion del punto del usuario en el centro del mapa.

--- js/geolocalizr.js
+++ js/geolocalizr.js
@@ -5,19 +5,66 @@
  *
  */	
 
-//TODO: lo primero que debe evaluarse es la posicion geo , dependiendo de esto se agrega un elemento
-// al inicio del json , o no. De acuerdo a si esta disponible o no la geolocalizacion deberia llamar al 
-// metodo initializeNewMap
-
-var latInit, lngInit;
-var geoFlag = 0; 
+//TODO: tengo que crear un metodo para la geolocalizacion, luego dependiendo de  
 
 //Metodo que inicializa el mapa 
 function initializeNewMap(locations,zoom){
 
-	console.log("inicializo mapa")
+	var map = new google.maps.Map(document.getElementById('googleMap'), {
+      zoom: zoom,
+      center: new google.maps.LatLng(locations[0]["lat"], locations[0]["lng"]),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
 
-	//aca pongo el codigo de geo html5 ?
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    console.log("recorro locations")
+    for (i = 0; i < locations.length; i++) {  
+    	console.log(i)
+		marker = new google.maps.Marker({
+		position: new google.maps.LatLng(locations[i]["lat"], locations[i]["lng"]),
+		map: map,
+		icon: 'http://ktct.ca/images/gis/map_icon_poi.png'
+		});
+
+		google.maps.event.addListener(marker, 'click', (function(marker, i) {
+		return function() {
+
+			var contentString = '<div id="containerInfoMap" style="width: 300px; height: 200px;">' +
+				'<div class="img" style="text-align: center; background-color: ' + locations[i]["bg-color"] +';"><img src="' + locations[i]["img"]  + '"></img></div>' +
+				'<div class="title" style="text-align: center;"><h3>' + locations[i]["title"] + '</h3></div>' +
+				'<div class="description" style="text-align: center;">' +
+				'<p>' + locations[i]["description"] + '</p>' +
+				'</div>' +
+				'<div class="phone" style="text-align: center;"><strong>Tel: </strong>' + locations[i]["phone"] + '</div>' +
+				'<div class="email" style="text-align: center;"><strong>Email: </strong>' + locations[i]["email"] + '</div>' +
+				'</div>';			            	
+
+			infowindow.setContent(contentString);
+			infowindow.open(map, marker);
+		}
+		})(marker, i));
+
+    }
+
+	console.log("inicializo mapa")
+	   
+}
+
+//Metodo que bindea el mapa
+function bindGoogleMaps(points,zoom){
+	google.maps.event.addDomListener(window, 'load', initializeNewMap(points,zoom));
+}
+
+//Metodo que valida geolocalizacion y obtiene coordenadas del usuario
+
+function getGeolocalization(callback){
+
+	console.log("Cargamos el mapa cuando cargue el DOM");
+
+    //aca pongo el codigo de geo html5 ?
 	if(navigator.geolocation){
 		//intentamos obtener las coordenadas del usuario
 		navigator.geolocation.getCurrentPosition(function(objPosicion){
@@ -36,6 +83,7 @@ function initializeNewMap(locations,zoom){
 				latInit = latAuto;
 				lngInit = lngAuto;
 				console.log("Agrego el elemento");
+				
 				locations.splice(0, 0,
 				    {
 						"lat" 			: latInit,
@@ -47,49 +95,10 @@ function initializeNewMap(locations,zoom){
 						"img"			: "",
 						"link"			: "",
 						"bg-color"		: "cornflowerblue" 
-					}
-				);
+					});
 			}
 			console.log("despues de evaluar flag");
-
-			var map = new google.maps.Map(document.getElementById('googleMap'), {
-		      zoom: zoom,
-		      center: new google.maps.LatLng(locations[0]["lat"], locations[0]["lng"]),
-		      mapTypeId: google.maps.MapTypeId.ROADMAP
-		    });
-
-		    var infowindow = new google.maps.InfoWindow();
-
-		    var marker, i;
-
-		    console.log("recorro locations")
-		    for (i = 0; i < locations.length; i++) {  
-		    	console.log(i)
-				marker = new google.maps.Marker({
-				position: new google.maps.LatLng(locations[i]["lat"], locations[i]["lng"]),
-				map: map,
-				icon: 'http://ktct.ca/images/gis/map_icon_poi.png'
-				});
-
-				google.maps.event.addListener(marker, 'click', (function(marker, i) {
-				return function() {
-
-					var contentString = '<div id="containerInfoMap" style="width: 300px; height: 200px;">' +
-						'<div class="img" style="text-align: center; background-color: ' + locations[i]["bg-color"] +';"><img src="' + locations[i]["img"]  + '"></img></div>' +
-						'<div class="title" style="text-align: center;"><h3>' + locations[i]["title"] + '</h3></div>' +
-						'<div class="description" style="text-align: center;">' +
-						'<p>' + locations[i]["description"] + '</p>' +
-						'</div>' +
-						'<div class="phone" style="text-align: center;"><strong>Tel: </strong>' + locations[i]["phone"] + '</div>' +
-						'<div class="email" style="text-align: center;"><strong>Email: </strong>' + locations[i]["email"] + '</div>' +
-						'</div>';			            	
-
-					infowindow.setContent(contentString);
-					infowindow.open(map, marker);
-				}
-				})(marker, i));
-
-		    }
+			return callback();
 
 		},function(objError){
 			//manejamos los errores devueltos por Geolocation API
@@ -108,29 +117,28 @@ function initializeNewMap(locations,zoom){
 				case objError.PERMISSION_DENIED:
 					geoFlag = 0;
 				    console.log("Acceso denegado.")
+				    return callback();
 				break;
 				//errores desconocidos
 				case objError.UNKNOWN_ERROR:
 					geoFlag = 0;
 					console.log("Error desconocido.");
+					return callback();
 				break;
 			}
 		});
+
 	}else{
 		//el navegador del usuario no soporta el API de Geolocalizacion de HTML5
 		geoFlag = 0;
 		console.log("Su navegador no soporta Geolocalizacion");
+		return callback();
 	}
 
-	
-	
+	//return callback();
 
-
-
-    
 }
 
-//Metodo que bindea el mapa
-function bindGoogleMaps(points,zoom){
-	google.maps.event.addDomListener(window, 'load', initializeNewMap(points,zoom));
+function pepe(){
+	console.log("pepe")
 }
