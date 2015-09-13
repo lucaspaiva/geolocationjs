3e5101a068f746ba9a87e287fe75386ac07bb4ca
Lucas Paiva <contacto.lucas.paiva@gmail.com>
2015-09-13 19:45:06 -0300

    fixes

--- js/geolocalizr.js
+++ js/geolocalizr.js
@@ -27,7 +27,7 @@ function initializeNewMap(locations,zoom){
 		marker = new google.maps.Marker({
 		position: new google.maps.LatLng(locations[i]["lat"], locations[i]["lng"]),
 		map: map,
-		icon: locations[i]["icon-point"]
+		icon: path_images + locations[i]["icon-point"]
 		});
 
 		google.maps.event.addListener(marker, 'click', (function(marker, i) {
